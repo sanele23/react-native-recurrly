ea648f2c425552c8fdd69d969ce751d924e140ce
Sanele Krakra <sanele23@live.com>
2026-04-08 14:15:00 +0200

    Initial commit: project setup and configuration

---
+++ app/onboarding.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+
+const Onboarding = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-background">
+      <Text className="text-xl font-bold text-success">
+        Welcome to the Onboarding Screen!
+      </Text>
+    </View>
+  );
+};
+
+export default Onboarding;
